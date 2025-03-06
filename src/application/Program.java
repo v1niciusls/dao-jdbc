014f86eb5b273376b4cb7eb8badecd391184d874
vinicinls <viniciuslacerda437@gmail.com>
2025-03-06 00:10:44 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,5 +22,14 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 3: seller findbyAll ====");
+
+        list = sellerDao.findAll();
+        for (Seller obj : list){
+            System.out.println(obj);
+        }
+
+
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -91,10 +91,6 @@ public class SellerDaoJDBC implements SellerDao {
         return department;
     }
 
-    @Override
-    public List<Seller> findAll() {
-        return null;
-    }
 
     @Override
     public List<Seller> findByDepartment(Department department) {
@@ -130,5 +126,43 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeStatement(st);
             DB.closeResultSet(rs);
         }
+    }
+
+    public List<Seller> findAll() {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "ORDER BY Name ");
+
+            rs = st.executeQuery();
+
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+                Department dep = map.get(rs.getInt("DepartmentId"));
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+                Seller seller1 = instantiateSeller(rs, dep);
+                list.add(seller1);
+
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+
     }
 }
